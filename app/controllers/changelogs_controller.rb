31b39d1e63e8d5c263505a25f487ea4ea2ca8faf
trowbridge.james@gmail.com <trowbridge.james@gmail.com>
2017-01-18 23:59:30 -0500

    order changelog correctly

--- app/controllers/changelogs_controller.rb
+++ app/controllers/changelogs_controller.rb
@@ -2,7 +2,7 @@ class ChangelogsController < ApplicationController
 
   skip_before_filter :authenticate_user!
   def index
-    @changelogs = Changelog.all
+    @changelogs = Changelog.all.order("created_at DESC")
   end
 
 end
