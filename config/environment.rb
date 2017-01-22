73bedc55b9939b9de63f898b6331549e4ba191f2
trowbridge.james@gmail.com <trowbridge.james@gmail.com>
2017-01-22 14:31:03 -0500

    add multiple domain session store to fix error with invites not working correctly

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-Rails3App::Application.config.session_store :cookie_store, :key => '_rails5_bdostats_app_session', :domain => :all
+Rails.application.config.session_store :cookie_store, :key => '_rails5_bdostats_app_session', :domain => :all
 # Initialize the Rails application.
 Rails.application.initialize!
